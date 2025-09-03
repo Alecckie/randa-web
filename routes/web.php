f7abde66a3bfd2d30dfbed40dcc9b34913658bb5
IzooYut <munyorokuisaac@gmail.com>
2025-09-03 12:08:47 +0300

    added randa home page

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,16 @@ use Inertia\Inertia;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 
 
-// Route::get('/', function () {
-//     return Inertia::render('Welcome', [
-//         'canLogin' => Route::has('login'),
-//         'canRegister' => Route::has('register'),
-//         'laravelVersion' => Application::VERSION,
-//         'phpVersion' => PHP_VERSION,
-//     ]);
-// });
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-Route::get('/',[AuthenticatedSessionController::class, 'create']);
+// Route::get('/',[AuthenticatedSessionController::class, 'create']);
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
