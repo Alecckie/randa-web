7eb8cc31695874c96f78f54bec37f4bbcee36bf3
IzooYut <munyorokuisaac@gmail.com>
2026-02-17 09:58:59 +0300

    fixed wrong namespace naming on location form request

--- app/Http/Controllers/Api/RiderTrackingController.php
+++ app/Http/Controllers/Api/RiderTrackingController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Rider\StoreLocationRequest;
 use App\Http\Requests\StoreBatchLocationRequest;
+use App\Http\Requests\StoreLocationRequest;
 use App\Services\RiderTrackingService;
 use App\Services\RiderService;
 use Illuminate\Http\Request;
